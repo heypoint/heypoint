a2f18c368f8b33eb9fd02064c8845d3969ef397e
Gavin Sawyer <g@vinsawyer.dev>
2023-08-24 23:48:12 -0400

    v0.0.0
    - Linting fix.

--- libs/cloud-functions/src/lib/hello-world.ts
+++ libs/cloud-functions/src/lib/hello-world.ts
@@ -1,7 +1,6 @@
 import { onCall } from "firebase-functions/v2/https";
 
 
-// noinspection JSUnusedGlobalSymbols
 export const helloWorld: CallableFunction = onCall<void, string>(
   {
     enforceAppCheck: true,
